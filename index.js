e47ed47665a563ce68c32ea2b8a426abe102b731
Muhammad Bilal Mohib-ul-Nabi <bilalmohib7896@gmail.com>
2022-07-09 09:51:16 +0500

    For Google Cloud

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// Copyright 2017 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
 const express = require("express");
 const pool = require("./database");
 const app = express();
@@ -223,7 +239,7 @@ app.get("/book", async (req, res) => {
         });
     res.status(200).json(book);
 });
- 
+
 //To get specific book with given id @Tested
 app.get("/book/:id", async (req, res) => {
     const id = req.params.id;
